ef74d64e460465e1fd5bb72f31999ded9e5af3fd
antonio santos <antoniosantos.devweb@gmail.com>
2024-09-09 16:12:11 -0300

    library fuction add

--- src/Pages/Library.jsx
+++ src/Pages/Library.jsx
@@ -4,6 +4,7 @@ import './css/Library.css'; // Certifique-se de criar o arquivo Library.css
 function Library() {
   const [library, setLibrary] = useState([]);
   const [bookClub, setBookClub] = useState({ name: '', book: {}, members: [] });
+  const [selectedBook, setSelectedBook] = useState(null); // Estado para o livro selecionado
 
   // Função para buscar os dados da biblioteca do backend
   useEffect(() => {
@@ -18,18 +19,37 @@ function Library() {
       .catch(error => console.error('Erro ao buscar clube do livro:', error));
   }, []);
 
+  // Função para lidar com o clique no livro
+  const handleBookClick = (book) => {
+    setSelectedBook(book); // Define o livro selecionado
+  };
+
   return (
     <div className="library-container">
       <div className="section library">
         <h3>Sua biblioteca:</h3>
         <div className="books">
           {library.map((book, index) => (
-            <div key={index} className="book-cover">
+            <div 
+              key={index} 
+              className="book-cover" 
+              onClick={() => handleBookClick(book)} // Adiciona evento de clique
+            >
               <p>{book.name}</p>
             </div>
           ))}
         </div>
       </div>
+
+      {/* Exibir detalhes do livro selecionado */}
+      {selectedBook && (
+        <div className="book-details">
+          <h3>Detalhes do Livro</h3>
+          <p>Nome: {selectedBook.name}</p>
+          <p>Autor: {selectedBook.author}</p>
+          <p>Descrição: {selectedBook.description}</p>
+        </div>
+      )}
 
       <div className="section book-club">
         <h3>Clube do livro: {bookClub.name}</h3>
