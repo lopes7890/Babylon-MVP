a9a2e48fe9b34a4fb103dfa227444ba93ab7aab8
Matheus Souza <matheussouzaarcanjo444@gmail.com>
2024-10-16 09:09:08 -0300

    Remoção de alguns residuos esquecidos no ultimo push

--- src/Pages/LoginSinup.jsx
+++ src/Pages/LoginSinup.jsx
@@ -33,10 +33,6 @@ function LoginSignup() {
       ? 'https://babylon-mvp-backend.onrender.com/user'
       : 'https://babylon-mvp-backend.onrender.com/login';
 
-    // const url = isSignup
-    //   ? 'http://localhost:8080/user'
-    //   : 'http://localhost:8080/login';
-
     const body = JSON.stringify(formData);
 
     try {
