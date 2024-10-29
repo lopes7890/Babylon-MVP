66c0c32b4afd952dbe2886ef6ea05de569c9e678
Matheus Souza <matheussouzaarcanjo444@gmail.com>
2024-10-29 17:09:02 -0300

    Atualizacao na navrgacao pelo footer e ajuste na pagina workShop na largura da div projetos

--- src/Components/FooterNav/FooterNav.jsx
+++ src/Components/FooterNav/FooterNav.jsx
@@ -1,29 +1,46 @@
-import React from 'react';
+import React, { useCallback, useLayoutEffect, useRef, useState } from 'react';
 import './FooterNav.css';
-import live_icon from '../../assets/live_icon.png';
-import lapis from '../../assets/lapis.png';
-import home from '../../assets/home.png';
-import books from '../../assets/livros.png';
-import perfil from '../../assets/perfil.png';
 import { Link } from 'react-router-dom';
+import { FaHome, FaBook, FaPodcast, FaPencilAlt, FaUser } from 'react-icons/fa';
+
 function FooterNav() {
+  const [select, setSelect] = useState(1);
+
+  const background = useRef(undefined);
+
+  useLayoutEffect(() => {
+    if (background.current) {
+      background.current.style.transform = `translateX(${100 * select}%)`;
+    }
+  }, [select]);
+
+  const moveBackground = useCallback(({ target }) => {
+    const index = target.closest('a')?.getAttribute('index');
+    if (index) setSelect(Number(index));
+  });
+
   return (
-    <footer className="footer">
-      <Link to="/">
-        <img src={home} alt="" />
-      </Link>
-      <Link to="/library">
-        <img src={books} alt="" />
-      </Link>
-      <Link to="/lives">
-        <img src={live_icon} alt="" />
-      </Link>
-      <Link to="/workshop">
-        <img src={lapis} alt="" />
-      </Link>
-      <Link to="/profile">
-        <img src={perfil} alt="" />
-      </Link>
+    <footer className="footer-container">
+      <div className="footer" onClick={moveBackground}>
+        <div className="fundo" ref={background}></div>
+
+        <Link to="/" index="0">
+          <FaHome />
+        </Link>
+
+        <Link to="/library" index="1">
+          <FaBook />
+        </Link>
+        <Link to="/lives" index="2">
+          <FaPodcast />
+        </Link>
+        <Link to="/workshop" index="3">
+          <FaPencilAlt />
+        </Link>
+        <Link to="/profile" index="4">
+          <FaUser />
+        </Link>
+      </div>
     </footer>
   );
 }
